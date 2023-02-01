8125cc90145c5947d8b1364bdce2083002db231e
vieleo <nguyenquangviet2k1@gmail.com>
2023-02-01 14:20:19 +0700

    chay thu server

--- database/migrations/2023_01_08_184543_create_profiles_table.php
+++ database/migrations/2023_01_08_184543_create_profiles_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->bigIncrements('id');
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            // $table->text('avatar');
             $table->string('phone');
             $table->text('address');
             $table->integer('gender');
