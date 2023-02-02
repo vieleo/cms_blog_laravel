6cd4798722b233ffbf54bb11ba4f8518fda972e2
vieleo <nguyenquangviet2k1@gmail.com>
2023-02-02 16:47:37 +0700

    test nhanh

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -52,13 +52,9 @@ class ProfileController extends Controller
         $request->validateWithBag('userDeletion', [
             'password' => ['required', 'current-password'],
         ]);
-
         $user = $request->user();
-
         Auth::logout();
-
         $user->delete();
-
         $request->session()->invalidate();
         $request->session()->regenerateToken();
 
